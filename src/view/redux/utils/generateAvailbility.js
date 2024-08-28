8818757d97626fd49a21bf1da0639a81b0997b86
Dhairya jain <dhairyajain2617@gmail.com>
2024-08-28 13:49:46 +0530

    availability

--- src/view/redux/utils/generateAvailbility.js
+++ src/view/redux/utils/generateAvailbility.js
@@ -1,19 +1,23 @@
-// src/utils/generateAvailability.js
-export const generateAvailability = (daysInAdvance = 30) => {
-  // 30 
-    const availability = [];
-    const today = new Date();
-  
-    for (let i = 0; i < daysInAdvance; i++) {
-      const futureDate = new Date(today);
-      futureDate.setDate(today.getDate() + i);
-  
-      availability.push({
-        date: futureDate.toISOString().split('T')[0], // Format date as YYYY-MM-DD
-        available: true, // By default, rooms are available
-      });
-    }
-  
-    return availability;
-  };
-  
+export const generateAvailability = () => {
+  const availability = [];
+  const today = new Date();
+
+  // Get the current year and month
+  const year = today.getFullYear();
+  const month = today.getMonth(); // 0-indexed (January is 0, December is 11)
+
+  // Calculate the number of days in the current month
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  for (let i = 0; i < daysInMonth; i++) {
+    const futureDate = new Date(today);
+    futureDate.setDate(today.getDate() + i);
+
+    availability.push({
+      date: futureDate.toISOString().split('T')[0], // Format date as YYYY-MM-DD
+      available: true, // By default, rooms are available
+    });
+  }
+
+  return availability;
+};
