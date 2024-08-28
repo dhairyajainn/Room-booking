e76dbf8b2767af21faf70b682d24d307263e8ac1
Dhairya jain <dhairyajain2617@gmail.com>
2024-08-28 14:17:53 +0530

    assa

--- src/view/redux/utils/generateAvailbility.js
+++ src/view/redux/utils/generateAvailbility.js
@@ -2,20 +2,18 @@ export const generateAvailability = () => {
   const availability = [];
   const today = new Date();
 
-  // Get the current year and month
   const year = today.getFullYear();
-  const month = today.getMonth(); // 0-indexed (January is 0, December is 11)
+  const month = today.getMonth();
 
-  // Calculate the number of days in the current month
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const daysInMonth = new Date(year, month + 1, 0).getDate(); //month + 1 kyuknki months ka index 0 se hai 
 
   for (let i = 0; i < daysInMonth; i++) {
     const futureDate = new Date(today);
     futureDate.setDate(today.getDate() + i);
 
     availability.push({
-      date: futureDate.toISOString().split('T')[0], // Format date as YYYY-MM-DD
-      available: true, // By default, rooms are available
+      date: futureDate.toISOString().split('T')[0],
+      available: true, 
     });
   }
 
